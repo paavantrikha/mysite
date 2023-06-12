7ab2e63473dcb3885412ed4309593736fb8600da
Paavan Trikha <paavantrikha@gmail.com>
2023-06-12 14:55:03 +0530

    Model created

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name # To see item_name(string) instead of object1, object2 when we run Item.objects.all()
+
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
